6333f67a79927f3b399be2db4d7f0fbcfe2f5759
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2022-01-21 04:12:10 +0300

    new file

--- test.py
+++ test.py
@@ -76,14 +76,14 @@
 #     print (i)
 # 
 #  функция
-def f(x):
-    if x == 1:
-        return 'Целое'
-    elif x == 2.3:
-        return 23
-    else:
-        return 
+# def f(x):
+#     if x == 1:
+#         return 'Целое'
+#     elif x == 2.3:
+#         return 23
+#     else:
+#         return 
 
-arg = 2.3
-print (f(arg))
-print (type(f(arg)))        
+# arg = 2.3
+# print (f(arg))
+# print (type(f(arg)))        
