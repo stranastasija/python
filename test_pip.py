50b08ea619a8d1caca3c4a0decfdcc974b18fbeb
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2022-02-15 22:02:28 +0300

    pip

---
+++ test_pip.py
@@ -0,0 +1,8 @@
+# from progress.bar import FillingCirclesBar
+# import time
+
+# bar = FillingCirclesBar('Processing', max=20)
+# for i in range(20):
+#     time.sleep(1)
+#     bar.next()
+# bar.finish()
