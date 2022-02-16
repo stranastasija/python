70139791c655bba0a5c99e83935d00f464776083
AnastasiyaSTR <anastasija.striukova@yandex.ru>
2022-02-16 23:22:19 +0300

    correct file

--- bot_test.py
+++ bot_test.py
@@ -8,6 +8,10 @@ updater.dispatcher.add_handler(CommandHandler('hi', hi_command))
 updater.dispatcher.add_handler(CommandHandler('time', time_command))
 updater.dispatcher.add_handler(CommandHandler('help', help_command))
 updater.dispatcher.add_handler(CommandHandler('sum', sum_command))
+updater.dispatcher.add_handler(CommandHandler('dif', dif_command))
+updater.dispatcher.add_handler(CommandHandler('motive', motive_command))
+updater.dispatcher.add_handler(CommandHandler('love', love_command))
+updater.dispatcher.add_handler(CommandHandler('bye', bye_command))
 
 print ('server start')
 
